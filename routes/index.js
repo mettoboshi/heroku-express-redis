f2185fd96390e676f1b7f246dd252d0a2241b167
mettoboshi <mettoboshi.1108@gmail.com>
2014-11-25 15:26:06 +0900

    connected redis

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
+var redis = require('redis');
+var url = require('url');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  if (process.env.REDISTOGO_URL) {
+    // inside if statement
+    var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+    var redis = require("redis").createClient(rtg.port, rtg.hostname);
+    redis.auth(rtg.auth.split(":")[1]);
+  } else {
+    var redis = require('redis').createClient();
+  }
+
+  redis.set('key','value', function(){
+  });
+
+  redis.get('key', function(err, val){
+    if (err) return console.log(err);
+    redis.quit();
+    res.render('index', { title: val });
+  });
+  
 });
 
 module.exports = router;
